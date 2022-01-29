04e6162508a548a03c5456a5d54fd10d471b43bf
ThibFranzi <78700088+roubzi@users.noreply.github.com>
2022-01-29 16:18:57 +0100

    Init

---
+++ src/App.jsx
@@ -0,0 +1,187 @@
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Account from "components/Account/Account";
+import Chains from "components/Chains";
+import TokenPrice from "components/TokenPrice";
+import ERC20Balance from "components/ERC20Balance";
+import ERC20Transfers from "components/ERC20Transfers";
+import DEX from "components/DEX";
+import NFTBalance from "components/NFTBalance";
+import Wallet from "components/Wallet";
+import { Layout, Tabs } from "antd";
+import "antd/dist/antd.css";
+import NativeBalance from "components/NativeBalance";
+import "./style.css";
+import QuickStart from "components/QuickStart";
+import Contract from "components/Contract/Contract";
+import Text from "antd/lib/typography/Text";
+import Ramper from "components/Ramper";
+import MenuItems from "./components/MenuItems";
+const { Header, Footer } = Layout;
+
+const styles = {
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    fontFamily: "Roboto, sans-serif",
+    color: "#041836",
+    marginTop: "130px",
+    padding: "10px",
+  },
+  header: {
+    position: "fixed",
+    zIndex: 1,
+    width: "100%",
+    background: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontFamily: "Roboto, sans-serif",
+    borderBottom: "2px solid rgba(0, 0, 0, 0.06)",
+    padding: "0 10px",
+    boxShadow: "0 1px 10px rgb(151 164 175 / 10%)",
+  },
+  headerRight: {
+    display: "flex",
+    gap: "20px",
+    alignItems: "center",
+    fontSize: "15px",
+    fontWeight: "600",
+  },
+};
+const App = ({ isServerInfo }) => {
+  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
+    useMoralis();
+
+  useEffect(() => {
+    const connectorId = window.localStorage.getItem("connectorId");
+    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading)
+      enableWeb3({ provider: connectorId });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated, isWeb3Enabled]);
+
+  return (
+    <Layout style={{ height: "100vh", overflow: "auto" }}>
+      <Router>
+        <Header style={styles.header}>
+          <Logo />
+          <MenuItems />
+          <div style={styles.headerRight}>
+            <Chains />
+                        <TokenPrice
+              address="0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"
+              chain="eth"
+              //image="https://cloudflare-ipfs.com/ipfs/QmQTx2mZwnBgL4cYzasTZ834FA2kLNjtepjnscvWJSk7Jc/"
+              image="https://bafybeic3wfudggswwjdvuz5jnogwdy6cl7eozz3smudv5dxcdct5xdflam.ipfs.infura-ipfs.io/"
+              size = "40px"
+              //height="40px"
+              //width="40px"
+            />
+            <NativeBalance />
+            <Account />
+          </div>
+        </Header>
+
+        <div style={styles.content}>
+          <Switch>
+            <Route exact path="/quickstart">
+              <QuickStart isServerInfo={isServerInfo} />
+            </Route>
+            <Route path="/wallet">
+              <Wallet />
+            </Route>
+            <Route path="/1inch">
+              <Tabs defaultActiveKey="1" style={{ alignItems: "center" }}>
+                <Tabs.TabPane tab={<span>Ethereum</span>} key="1">
+                  <DEX chain="eth" />
+                </Tabs.TabPane>
+                <Tabs.TabPane tab={<span>Binance Smart Chain</span>} key="2">
+                  <DEX chain="bsc" />
+                </Tabs.TabPane>
+                <Tabs.TabPane tab={<span>Polygon</span>} key="3">
+                  <DEX chain="polygon" />
+                </Tabs.TabPane>
+              </Tabs>
+            </Route>
+            <Route path="/nftBalance">
+              <NFTBalance />
+            </Route>
+            <Route path="/erc20balance">
+              <ERC20Balance />
+            </Route>
+            <Route path="/onramp">
+              <Ramper />
+            </Route>
+            <Route path="/erc20transfers">
+              <ERC20Transfers />
+            </Route>
+            <Route path="/contract">
+              <Contract />
+            </Route>
+            <Route path="/">
+              <Redirect to="/quickstart" />
+            </Route>
+            <Route path="/ethereum-boilerplate">
+              <Redirect to="/quickstart" />
+            </Route>
+            <Route path="/nonauthenticated">
+              <>Please login using the "Authenticate" button</>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <Footer style={{ textAlign: "center" }}>
+        <Text style={{ display: "block" }}>
+          ⭐️ Please star this{" "}
+          <a
+            href="https://github.com/ethereum-boilerplate/ethereum-boilerplate/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            boilerplate
+          </a>
+          , every star makes us very happy!
+        </Text>
+
+        <Text style={{ display: "block" }}>
+          🙋 You have questions? Ask them on the {""}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://forum.moralis.io/t/ethereum-boilerplate-questions/3951/29"
+          >
+            Moralis forum
+          </a>
+        </Text>
+
+        <Text style={{ display: "block" }}>
+          📖 Read more about{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://roubzi.org"
+          >
+            Moralis
+          </a>
+        </Text>
+      </Footer>
+    </Layout>
+  );
+};
+
+
+
+export const Logo = () => (
+  <div style={{ display: "flex" }}>
+
+
+</div>
+);
+
+export default App;
---
+++ src/components/MenuItems.jsx
@@ -0,0 +1,46 @@
+import { useLocation } from "react-router";
+import { Menu } from "antd";
+import { NavLink } from "react-router-dom";
+
+function MenuItems() {
+  const { pathname } = useLocation();
+
+  return (
+    <Menu
+      theme="light"
+      mode="horizontal"
+      style={{
+        display: "flex",
+        fontSize: "17px",
+        fontWeight: "500",
+        width: "100%",
+        justifyContent: "center",
+      }}
+      defaultSelectedKeys={[pathname]}
+    >
+      <Menu.Item key="/quickstart">
+        <NavLink to="/quickstart">🔥 Informations</NavLink>
+      </Menu.Item>
+      <Menu.Item key="/wallet">
+        <NavLink to="/wallet">💰 Wallet</NavLink>
+      </Menu.Item>
+      <Menu.Item key="/1inch">
+        <NavLink to="/1inch">🌺 Dex</NavLink>
+      </Menu.Item>
+      <Menu.Item key="/nftBalance">
+        <NavLink to="/nftBalance">👾 RZIs & NFTs</NavLink>
+      </Menu.Item>
+      <Menu.Item key="/erc20balance">
+        <NavLink to="/erc20balance">🍯 Balances</NavLink>
+      </Menu.Item>
+      <Menu.Item key="/erc20transfers">
+        <NavLink to="/erc20transfers">🐝 Transfers</NavLink>
+      </Menu.Item>
+      <Menu.Item key="onramp">
+        <NavLink to="/onramp">💵 Fiat</NavLink>
+      </Menu.Item>
+      </Menu>
+  );
+}
+
+export default MenuItems;
