11525b651a9ec7aa324ce057b1dbc789e6eeef30
ThibFranzi <78700088+roubzi@users.noreply.github.com>
2022-02-09 12:03:37 +0100

    Correction des bugs d'affichage du menu
    Encodage de la fleur posait problème
    Recherche de la cause du problème et changement de caractère

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -18,31 +18,41 @@ function MenuItems() {
       }}
       defaultSelectedKeys={[pathname]}
     >
-      <Menu.Item key="/quickstart">
-        <NavLink to="/quickstart">🔥 Informations</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/wallet">
-        <NavLink to="/wallet">💰 Wallet</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/1inch">
-        <NavLink to="/1inch">🦠 Swap RZIG</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/RZIDex">
-        <NavLink to="/RZIDex">🌺 RziDex Encyclopedia </NavLink>
-      </Menu.Item>
-      <Menu.Item key="/nftBalance">
-        <NavLink to="/nftBalance">👾 RZIs & NFTs</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/erc20balance">
-        <NavLink to="/erc20balance">🍯 Balances</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/erc20transfers">
-        <NavLink to="/erc20transfers">🐝 Transfers</NavLink>
-      </Menu.Item>
-      <Menu.Item key="onramp">
-        <NavLink to="/onramp">💵 Fiat</NavLink>
-      </Menu.Item>
-      </Menu>
+
+    <Menu.Item key="/quickstart">
+      <NavLink to="/quickstart">🔥Informations</NavLink>
+    </Menu.Item>
+
+    <Menu.Item key="/wallet">
+      <NavLink to="/wallet">💰Wallet</NavLink>
+    </Menu.Item>
+
+    <Menu.Item key="/1inch">
+      <NavLink to="/1inch">🦠Swap RZIG</NavLink>
+    </Menu.Item>
+
+    <Menu.Item key="/RZIDex">
+      <NavLink to="/RZIDex">🎣RziDex </NavLink>
+    </Menu.Item>
+
+    <Menu.Item key="/nftBalance">
+      <NavLink to="/nftBalance">👾RZIs collection</NavLink>
+    </Menu.Item>
+
+    <Menu.Item key="/erc20balance">
+      <NavLink to="/erc20balance">🍯Balances</NavLink>
+    </Menu.Item>
+
+    <Menu.Item key="/erc20transfers">
+      <NavLink to="/erc20transfers">🐝Transfers</NavLink>
+    </Menu.Item>
+
+    <Menu.Item key="onramp">
+      <NavLink to="/onramp">💵Fiat</NavLink>
+    </Menu.Item>
+
+
+    </Menu>
   );
 }
 
